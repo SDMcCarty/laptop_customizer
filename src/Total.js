762fba7b22581d102057887d99fc0cca6100d9be
Sherry McCarty <sherrydmccarty@gmail.com>
2020-05-26 14:02:39 -0400

    cleaned up the Totals

--- src/Total.js
+++ src/Total.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 function Total(props) {
-  //calculates total
-  console.log(props)
   const total = Object.keys(props.selected).reduce(
     (acc, curr) => acc + props.selected[curr].cost,
     0
   );
-  console.log(total);
+
   return (
-    // <div></div>
     <div>{props.currencyFormat.format(total)}</div>
   )
 }
--- src/TotalPrice.js
+++ src/TotalPrice.js
@@ -17,11 +17,7 @@ function TotalPrice(props) {
       </div>
     );
   });
-// //calculates total
-//   const total = Object.keys(props.selected).reduce(
-//     (acc, curr) => acc + props.selected[curr].cost,
-//     0
-//   );
+  
   return (
     <section className="main__summary">
     <h2>Your cart</h2>
@@ -29,7 +25,6 @@ function TotalPrice(props) {
     <div className="summary__total">
       <div className="summary__total__label">Total</div>
       <div className="summary__total__value">
-        {/* {props.currencyFormat.format(total)} */}
         <Total 
           currencyFormat={props.currencyFormat}
           selected={props.selected}
